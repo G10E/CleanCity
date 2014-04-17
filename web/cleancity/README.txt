6f3eb2cd53ffeedd0eae2bf033964ca7e3ed1cf0
banmedo <ban.medo@live.com>
2014-04-17 15:41:30 +0545

    sql file added

---
+++ web/cleancity/README.txt
@@ -0,0 +1,5 @@
+For database the database is in PostgreSQL
+
+database name = 'cleancity'
+
+then the sql for generating table is in file 'TABLE CREATE.sql'
---
+++ web/cleancity/TABLE CREATE.sql
@@ -0,0 +1,11 @@
+﻿DROP TABLE reports;
+
+CREATE TABLE reports
+(
+  rep_id serial NOT NULL PRIMARY KEY,
+  rep_type character(25),
+  rep_comment text,
+  latitude character(20),
+  longitude character(20),
+  imgpath text
+);
